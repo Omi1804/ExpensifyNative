f94a09849b885259362ca799dc74bee5f37238ba
Om Nigam <om2021064@akgec.ac.in>
2024-06-20 10:10:10 +0530

    initalizing project intro

---
+++ Navigation/appNavigation.js
@@ -0,0 +1,11 @@
+import {StyleSheet, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+
+const appNavigation = () => {
+  return <NavigationContainer></NavigationContainer>;
+};
+
+export default appNavigation;
+
+const styles = StyleSheet.create({});
