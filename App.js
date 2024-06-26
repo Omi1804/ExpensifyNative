26ea76a247450953d3f4b7aa12aba6bb955205b4
Om Nigam <om2021064@akgec.ac.in>
2024-06-27 02:49:57 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View className="bg-red-500">
+      <Text className="font-extrabold">App</Text>
+    </View>
+  );
+};
+
+export default App;
